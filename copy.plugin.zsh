7c2f9d66d6e94a1bb5caed8fa5b873df3b889766
Xh4hn <89944287+Xh4hn@users.noreply.github.com>
2021-12-28 21:51:26 -0600

    Add files via upload

---
+++ copy.plugin.zsh
@@ -0,0 +1,17 @@
+# Simple plugin for CopyPaste 
+# Author: Xh4hn
+
+function cpf(){
+    xclip-copyfile $1
+}
+
+function ctf(){
+    xclip-cutfile $1
+}
+function ptf(){
+    xclip-pastefile
+}
+
+function cpof(){
+    catn $1 | xclip -sel clip
+}
